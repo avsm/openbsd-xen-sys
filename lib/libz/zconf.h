23ed43f85cd0ff7f40e04520a0b68a292e00f7a1
millert <devnull@localhost>
1998-09-08 03:32:04 +0000

    Update from zlib-1.1.3.  These should really just be references to ../../../lib/libz/*

--- lib/libz/zconf.h
+++ lib/libz/zconf.h
@@ -1,9 +1,9 @@
-/* $OpenBSD: zconf.h,v 1.1 1997/02/06 02:56:55 downsj Exp $ */
 /* zconf.h -- configuration of the zlib compression library
  * Copyright (C) 1995-1996 Jean-loup Gailly.
  * For conditions of distribution and use, see copyright notice in zlib.h 
  */
 
+/* $Id: zconf.h,v 1.3 1998/09/08 03:32:04 millert Exp $ */
 
 #ifndef _ZCONF_H
 #define _ZCONF_H
