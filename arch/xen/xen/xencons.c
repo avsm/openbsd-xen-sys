720f62d807dc105f36a1018331837fb22814ac49
egger@localhost.genua.de <egger@localhost.genua.de>
2006-06-23 09:31:32 +0200

    Initial import of Xen Version 2. This is the base for the work on Xen 3.

---
+++ arch/xen/xen/xencons.c
@@ -0,0 +1,546 @@
+/*	$NetBSD: xencons.c,v 1.8 2005/04/20 22:01:24 bouyer Exp $	*/
+
+/*
+ *
+ * Copyright (c) 2004 Christian Limpach.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *      This product includes software developed by Christian Limpach.
+ * 4. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+#include <sys/param.h>
+#include <sys/proc.h>
+#include <sys/tty.h>
+#include <sys/systm.h>
+#include <sys/device.h>
+#include <sys/conf.h>
+#include <sys/poll.h>
+
+#include <machine/xen.h>
+#include <machine/hypervisor.h>
+#include <machine/evtchn.h>
+#include <machine/ctrl_if.h>
+
+#include <dev/cons.h>
+
+#ifdef DDB
+#include <ddb/db_var.h>	/* XXX for db_max_line */
+#endif
+
+static int xencons_isconsole = 0;
+static struct xencons_softc *xencons_console_device = NULL;
+
+#define	XENCONS_UNIT(x)	(minor(x))
+#define XENCONS_BURST 128
+
+int		 xencons_match(struct device *, void*, void *);
+void		 xencons_attach(struct device *, struct device *, void *);
+int		 xencons_intr (void *);
+void		 xencons_tty_input(struct xencons_softc *, char*, int);
+
+int		 xenconsopen(dev_t, int, int, struct proc *);
+int		 xenconsclose(dev_t, int, int, struct proc *);
+int		 xenconsread(dev_t, struct uio *, int);
+int		 xenconswrite(dev_t, struct uio *, int);
+int		 xenconsioctl(dev_t, u_long, caddr_t, int, struct proc *);
+int		 xenconsstop(struct tty *, int);
+struct tty	*xenconstty(dev_t);
+int		 xenconspoll(dev_t, int, struct proc *);
+
+struct xencons_softc {
+	struct device	 sc_dev;
+	struct tty	*sc_tty;
+	int		 polling;
+	/* circular buffer when polling */
+	char		 buf[XENCONS_BURST];
+	volatile int	 buf_write;
+	volatile int	 buf_read;
+};
+
+struct cfattach	xencons_ca = {
+	sizeof(struct xencons_softc), xencons_match, xencons_attach,
+	NULL, NULL
+};
+
+struct cfdriver xencons_cd = {
+	NULL, "xencons", DV_TTY, 0, 0
+};
+
+const struct cdevsw xencons_cdevsw = {
+	xenconsopen, xenconsclose, xenconsread, xenconswrite,
+	xenconsioctl, xenconsstop, xenconstty, xenconspoll,
+	NULL, D_TTY, D_KQFILTER, ttkqfilter
+};
+
+
+static void	xencons_rx(ctrl_msg_t *, unsigned long);
+int		xenconscn_getc(dev_t);
+void		xenconscn_putc(dev_t, int);
+void		xenconscn_pollc(dev_t, int);
+
+static struct consdev xencons = {
+	NULL, NULL, xenconscn_getc, xenconscn_putc, xenconscn_pollc,
+	NULL, NODEV, CN_NORMAL
+};
+
+#if 0
+static struct cnm_state xencons_cnm_state;
+#endif
+
+void		xenconsstart (struct tty *);
+int		xenconsparam (struct tty *, struct termios *);
+
+int
+xencons_match(struct device *parent, void *match, void *aux)
+{
+	struct xencons_attach_args *xa = (struct xencons_attach_args *)aux;
+
+	if (strcmp(xa->xa_device, "xencons") == 0)
+		return 1;
+	return 0;
+}
+
+void
+xencons_attach(struct device *parent, struct device *self, void *aux)
+{
+	struct xencons_softc *sc = (void *)self;
+
+	printf(": Xen Virtual Console Driver\n");
+
+	sc->sc_tty = ttymalloc();
+#if 0
+	tty_attach(sc->sc_tty);
+#endif
+	sc->sc_tty->t_oproc = xenconsstart;
+	sc->sc_tty->t_param = xenconsparam;
+
+	if (xencons_isconsole) {
+		int maj;
+
+		/* Locate the major number. */
+		for (maj = 0; maj < nchrdev; maj++)
+			if (cdevsw[maj].d_open == xenconsopen)
+				break;
+
+		/* There can be only one, but it can have any unit number. */
+		cn_tab->cn_dev = makedev(maj, sc->sc_dev.dv_unit);
+
+		printf("%s: console major %d, unit %d\n", sc->sc_dev.dv_xname,
+		    maj, sc->sc_dev.dv_unit);
+
+		sc->sc_tty->t_dev = cn_tab->cn_dev;
+
+#ifdef DDB
+		/* Set db_max_line to avoid paging. */
+		db_max_line = 0x7fffffff;
+#endif
+
+		if (xen_start_info.flags & SIF_INITDOMAIN) {
+			int evtch = bind_virq_to_evtch(VIRQ_CONSOLE);
+			printf("%s: using event channel %d\n",
+			    sc->sc_dev.dv_xname, evtch);
+			if (event_set_handler(evtch, xencons_intr, sc,
+			    IPL_TTY, "xencons") != 0)
+				printf("console: can't register xencons_intr\n");
+			hypervisor_enable_event(evtch);
+		} else {
+			(void)ctrl_if_register_receiver(CMSG_CONSOLE,
+			    xencons_rx, 0);
+		}
+		xencons_console_device = sc;
+	}
+	sc->polling = 0;
+}
+
+int
+xenconsopen(dev_t dev, int flag, int mode, struct proc *p)
+{
+	struct tty *tp;
+
+	tp = xenconstty(dev);
+	if (tp == NULL)
+		return (ENXIO);
+
+	if ((tp->t_state & TS_ISOPEN) == 0) {
+		tp->t_dev = dev;
+		ttychars(tp);
+		tp->t_iflag = TTYDEF_IFLAG;
+		tp->t_oflag = TTYDEF_OFLAG;
+		tp->t_cflag = TTYDEF_CFLAG;
+		tp->t_lflag = TTYDEF_LFLAG;
+		tp->t_ispeed = tp->t_ospeed = TTYDEF_SPEED;
+		xenconsparam(tp, &tp->t_termios);
+		ttsetwater(tp);
+	} else if (tp->t_state&TS_XCLUDE && p->p_ucred->cr_uid != 0)
+		return (EBUSY);
+	tp->t_state |= TS_CARR_ON;
+
+	return ((*linesw[tp->t_line].l_open)(dev, tp));
+}
+
+int
+xenconsclose(dev_t dev, int flag, int mode, struct proc *p)
+{
+	struct tty *tp = xenconstty(dev);
+
+	if (tp == NULL)
+		return (0);
+	(*linesw[tp->t_line].l_close)(tp, flag);
+	ttyclose(tp);
+#ifdef notyet /* XXX */
+	ttyfree(tp);
+#endif
+	return (0);
+}
+
+int
+xenconsread(dev_t dev, struct uio *uio, int flag)
+{
+	struct tty *tp = xenconstty(dev);
+
+	return ((*linesw[tp->t_line].l_read)(tp, uio, flag));
+}
+
+int
+xenconswrite(dev_t dev, struct uio *uio, int flag)
+{
+	struct tty *tp = xenconstty(dev);
+
+	return ((*linesw[tp->t_line].l_write)(tp, uio, flag));
+}
+
+
+int
+xenconspoll(dev_t dev, int events, struct proc *p)
+{
+#ifdef __NetBSD__
+	struct tty *tp = xenconstty(dev);
+	return ((*linesw[tp->t_line].l_poll)(tp, events, p));
+#endif
+#ifdef __OpenBSD__
+	return ttpoll(dev, events, p);
+#endif
+}
+
+struct tty *
+xenconstty(dev_t dev)
+{
+	struct xencons_softc *sc =
+	    (struct xencons_softc *)device_lookup(&xencons_cd,
+	    XENCONS_UNIT(dev));
+	struct tty *tp = sc->sc_tty;
+
+	return (tp);
+}
+
+int
+xenconsioctl(dev_t dev, u_long cmd, caddr_t data, int flag, struct proc *p)
+{
+	struct tty *tp = xenconstty(dev);
+	int error;
+
+	error = (*linesw[tp->t_line].l_ioctl)(tp, cmd, data, flag, p);
+	if (error >= 0)
+		return (error);
+
+	error = ttioctl(tp, cmd, data, flag, p);
+	if (error >= 0)
+		return (error);
+
+	switch (cmd) {
+	default:
+		return (ENOTTY);
+	}
+
+#ifdef DIAGNOSTIC
+	panic("xencons_ioctl: impossible");
+#endif
+}
+
+void
+xenconsstart(struct tty *tp)
+{
+	struct clist *cl;
+	int s, len;
+
+	s = spltty();
+	if (tp->t_state & (TS_TIMEOUT | TS_BUSY | TS_TTSTOP))
+		goto out;
+	tp->t_state |= TS_BUSY;
+	splx(s);
+
+	/*
+	 * We need to do this outside spl since it could be fairly
+	 * expensive and we don't want our serial ports to overflow.
+	 */
+	cl = &tp->t_outq;
+	if (xen_start_info.flags & SIF_INITDOMAIN) {
+		int r;
+		u_char buf[XENCONS_BURST+1];
+
+		len = q_to_b(cl, buf, XENCONS_BURST);
+		while (len > 0) {
+			r = HYPERVISOR_console_io(CONSOLEIO_write, len, buf);
+			if (r <= 0)
+				break;
+			len -= r;
+		}
+	} else {
+		ctrl_msg_t msg;
+
+		len = q_to_b(cl, msg.msg, sizeof(msg.msg));
+		msg.type = CMSG_CONSOLE;
+		msg.subtype = CMSG_CONSOLE_DATA;
+		msg.length = len;
+		while (ctrl_if_send_message_noblock(&msg, NULL, 0) == EAGAIN) {
+			HYPERVISOR_yield();
+			/* XXX check return value and queue wait for space
+			 * thread/softint */
+		}
+	}
+
+	s = spltty();
+	tp->t_state &= ~TS_BUSY;
+	if (cl->c_cc) {
+		tp->t_state |= TS_TIMEOUT;
+#ifdef __NetBSD__
+		callout_reset(&tp->t_rstrt_ch, 1, ttrstrt, tp);
+#endif
+#ifdef __OpenBSD__
+		timeout_set(&tp->t_rstrt_to, ttrstrt, tp);
+		timeout_add(&tp->t_rstrt_to, 1);
+#endif
+	}
+	if (cl->c_cc <= tp->t_lowat) {
+		if (tp->t_state & TS_ASLEEP) {
+			tp->t_state &= ~TS_ASLEEP;
+			wakeup(cl);
+		}
+		selwakeup(&tp->t_wsel);
+	}
+out:
+	splx(s);
+}
+
+int
+xenconsstop(struct tty *tp, int flag)
+{
+	return (0);
+}
+
+
+/* Non-privileged receive callback. */
+static void
+xencons_rx(ctrl_msg_t *msg, unsigned long id)
+{
+	int i;
+	int s;
+	struct xencons_softc *sc;
+
+	sc = (struct xencons_softc *)device_lookup(&xencons_cd,
+	    XENCONS_UNIT(cn_tab->cn_dev));
+	if (sc == NULL)
+		goto out2;
+
+	s = spltty();
+	if (sc->polling) {
+		for (i = 0; i < msg->length; i++) {
+#if 0
+			cn_check_magic(sc->sc_tty->t_dev, msg->msg[i],
+			    xencons_cnm_state);
+#endif
+			sc->buf[sc->buf_write] = msg->msg[i];
+			sc->buf_write++;
+			if (sc->buf_write == XENCONS_BURST)
+				sc->buf_write = 0;
+			if (sc->buf_write == sc->buf_read) {
+				/*
+				 * we overflowed the circular buffer
+				 * advance the read pointer, meaning
+				 * we loose one char at the beggining
+				 * of the buf
+				 */
+				sc->buf_read++;
+				if (sc->buf_read == XENCONS_BURST)
+					sc->buf_read = 0;
+			}
+		}
+		goto out;
+	}
+
+	xencons_tty_input(sc, msg->msg, msg->length);
+out:
+	splx(s);
+out2:
+	msg->length = 0;
+	ctrl_if_send_response(msg);
+}
+
+void
+xencons_tty_input(struct xencons_softc *sc, char* buf, int len)
+{
+	struct tty *tp;
+	int i;
+
+	tp = sc->sc_tty;
+	if (tp == NULL)
+		return;
+
+	for (i = 0; i < len; i++) {
+#if 0
+		cn_check_magic(sc->sc_tty->t_dev, buf[i], xencons_cnm_state);
+#endif
+		(*linesw[tp->t_line].l_rint)(buf[i], tp);
+	}
+}
+
+/* privileged receive callback */
+int
+xencons_intr(void *p)
+{
+	static char rbuf[16];
+	int len;
+	struct xencons_softc *sc = p;
+
+	if (sc->polling)
+		return 1;
+
+	while ((len =
+	    HYPERVISOR_console_io(CONSOLEIO_read, sizeof(rbuf), rbuf)) > 0) {
+		xencons_tty_input(sc, rbuf, len);
+	}
+	return 1;
+}
+
+void
+xenconscn_attach(void)
+{
+	cn_tab = &xencons;
+
+	ctrl_if_early_init();
+
+#if 0
+	cn_init_magic(&xencons_cnm_state);
+	cn_set_magic("+++++");
+#endif
+
+	xencons_isconsole = 1;
+}
+
+int
+xenconscn_getc(dev_t dev)
+{
+	int ret;
+	char c;
+	int s = spltty();
+
+	if (xencons_console_device && xencons_console_device->polling == 0) {
+		printf("xenconscn_getc() but not polling\n");
+		splx(s);
+		return 0;
+	}
+	if (xen_start_info.flags & SIF_INITDOMAIN) {
+		while (HYPERVISOR_console_io(CONSOLEIO_read, 1, &c) == 0)
+			;
+#if 0
+		cn_check_magic(dev, c, xencons_cnm_state);
+#endif
+		splx(s);
+		return c;
+	}
+	if (xencons_console_device == NULL) {
+		printf("xenconscn_getc(): not console\n");
+		for (;;) {
+			/* loop here instead of in ddb */
+			HYPERVISOR_yield();
+		}
+		splx(s);
+		return 0;
+	}
+
+	while (xencons_console_device->buf_write ==
+	    xencons_console_device->buf_read) {
+		ctrl_if_console_poll();
+	}
+
+	ret = xencons_console_device->buf[xencons_console_device->buf_read];
+	xencons_console_device->buf_read++;
+	if (xencons_console_device->buf_read == XENCONS_BURST)
+		xencons_console_device->buf_read = 0;
+	splx(s);
+	return ret;
+}
+
+void
+xenconscn_putc(dev_t dev, int c)
+{
+	extern int ctrl_if_evtchn;
+	int s = spltty();
+
+	if (xen_start_info.flags & SIF_INITDOMAIN ||
+		ctrl_if_evtchn == -1) {
+		u_char buf[1];
+
+		buf[0] = c;
+		(void)HYPERVISOR_console_io(CONSOLEIO_write, 1, buf);
+	} else {
+		ctrl_msg_t msg;
+
+		msg.type = CMSG_CONSOLE;
+		msg.subtype = CMSG_CONSOLE_DATA;
+		msg.length = 1;
+		msg.msg[0] = c;
+		while (ctrl_if_send_message_noblock(&msg, NULL, 0) == EAGAIN) {
+			ctrl_if_console_poll();
+		}
+	}
+	splx(s);
+}
+
+void
+xenconscn_pollc(dev_t dev, int on)
+{
+	if (xencons_console_device)
+		xencons_console_device->polling = on;
+	if (on) {
+		xencons_console_device->buf_write = 0;
+		xencons_console_device->buf_read = 0;
+	}
+}
+
+/*
+ * Set line parameters.
+ */
+int
+xenconsparam(struct tty *tp, struct termios *t)
+{
+	tp->t_ispeed = t->c_ispeed;
+	tp->t_ospeed = t->c_ospeed;
+	tp->t_cflag = t->c_cflag;
+
+	return (0);
+}
